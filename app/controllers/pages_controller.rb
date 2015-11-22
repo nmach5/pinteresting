c00d8511d998dfdf0c8d192f0a003c7c079a23a7
Alp Er <alp_er515@hotmail.com>
2015-11-22 02:02:03 +0200

    cv added

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,6 @@ class PagesController < ApplicationController
   end
   def about
   end
+  def cv
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root "pages#home"
   get "about" => "pages#about"   # creates about_path
-
+  get "cv" => "pages#cv"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
